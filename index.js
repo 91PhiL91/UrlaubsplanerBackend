52d91c498f576560f5fb1c89f7e5ce781e9a94e2
PhiL <--->
2023-03-15 19:51:16 +0100

    Backend Test Route über Postman erreichbar

--- index.js
+++ index.js
@@ -2,20 +2,21 @@ const express = require('express'); // Importieren des Express-Frameworks
 const app = express(); // Erstellen einer Express-App
 const User = require('./controller/models/userModel')
 const Team = require('./controller/models/teamModel')
+const service = require('./controller/serviceController')
 const router = express.Router();
 
 
   
  
-  app.get('/api/user', (req, res) => {
-    User.findAll().then(user => {
-      console.log(user);
-      res.send(user);
-    }).catch((err) => {
-      console.error('Unable to query users:', err);
-      res.sendStatus(500);
-    });
-  });
+  // app.get('/api/user', (req, res) => {
+  //   User.findAll().then(user => {
+  //     console.log(user);
+  //     res.send(user);
+  //   }).catch((err) => {
+  //     console.error('Unable to query users:', err);
+  //     res.sendStatus(500);
+  //   });
+  // });
 
 
 //  Team.findAll().then((team)=>{
@@ -29,8 +30,9 @@ const router = express.Router();
 //     console.error('Unable to query users:', err);
 //   });
 
+module.exports = router; 
 
-
+app.use('/', service);
 
 app.listen(3000, () => { // Starten des Servers auf Port 3000
   console.log('Server started on port 3000'); // Ausgabe in der Konsole
